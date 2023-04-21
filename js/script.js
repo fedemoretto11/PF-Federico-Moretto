4c1d2dbdc65ce7a16bb40a73a67c676a6ccbc192
fedemoretto11 <fedemoretto94@gmail.com>
2023-04-20 23:27:34 -0300

    Funcion del carrousel rehecha con fetch y creacion de JSON para las variables del mismo

--- js/script.js
+++ js/script.js
@@ -12,32 +12,7 @@ const marcaSecundaria4 = document.getElementById("marcaSecundaria4");
 const marcaSecundaria5 = document.getElementById("marcaSecundaria5");
 
 
-// Variables contenido Carrousel
-let contador = 0;
-const imagenes = [
-  "img/productos.jpg", 
-  "img/productosAlimentos.jpg", 
-  "img/tokin2.jpg",
-  "img/sales.jpg",
-  "img/salsati.jpg",
-  "img/filosofia.jpg"
-];
-const titulos = [
-  "Algun Titulo", 
-  "Mision", 
-  "Vision",
-  "VALORES: LOS CLIENTES",
-  "VALORES: EL PERSONAL",
-  "VALORES: LA COMUNIDAD"
-];
-const textos = [
-  "Alguna breve descripcion que cuente lo que dice el titulo, pero cortita", 
-  "Visitar todas la bocas de expendio minoristas, logrando una efectividad de atencion del 95%",
-  "Ser el proveedor preferido en servicio de comercializacion y distribucion de productos de consumo masivo de la zona",
-  "Lograr la plena satisfacción de los mismos. Los principales objetivos de la organización hacia sus clientes son: cercanía, calidez, responsabilidad, respeto, servicio y comunicación",
-  "Conservar un vínculo armónico y estable en un marco de tolerancia, estabilidad, crecimiento, compromiso, respeto, reconocimiento, incentivo y comunicación constante. Son los principales pilares que la empresa sostiene con su personal, ya que se cuida mucho los Recursos Humanos",
-  "Seguir ayudando a los miembros de la comunidad con solidaridad y amor"
-];
+
 
 // Variables contenido marcas secundariass
 
@@ -57,24 +32,33 @@ const imagenesMarcas = [
 
 // Funcion Carrousel
 
+let contador = 0;
+
 function cambiarContendio() {
-  carrousel.classList.add("animate__fadeOutLeft");
-  setTimeout(() => {
-    contador++;
-    if (contador >= imagenes.length) {
-      contador = 0;
-    }
-    imagenCarrousel.src = imagenes[contador];
-    tituloCarrousel.innerHTML = titulos[contador];
-    textoCarrousel.innerHTML = textos[contador];
-
-    carrousel.classList.remove("animate__fadeOutLeft");
-
-    carrousel.classList.add("animate__fadeInRight");
+  fetch('json/datosCarrousel.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    
+    carrousel.classList.add("animate__fadeOutLeft");
     setTimeout(() => {
-      carrousel.classList.remove("animate__fadeInRight");
-    },1000)
-  },1000)
+      contador++;
+      if (contador >= data.length) {
+        contador = 0;
+      }
+      imagenCarrousel.src = data[contador].imagen;
+      imagenCarrousel.setAttribute("alt", data[contador].altImagen);
+      tituloCarrousel.innerHTML = data[contador].titulo;
+      textoCarrousel.innerHTML = data[contador].texto;
+      carrousel.classList.remove("animate__fadeOutLeft");
+
+      carrousel.classList.add("animate__fadeInRight");
+      setTimeout(() => {
+        carrousel.classList.remove("animate__fadeInRight");
+      }, 1000)
+    }, 1000)
+  })
+  .catch(error => console.error(error));
 }
 
 // Funcion cambio marcas
@@ -98,8 +82,11 @@ function cambiarMarcas() {
 }
 
 
+
+
+
+
 // Llamado funciones
 
 setInterval(cambiarContendio, 8000);
-setInterval(cambiarMarcas, 2500);
-
+// setInterval(cambiarMarcas, 2500);
